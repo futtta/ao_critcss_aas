8fae6c60530ad0d6d0699fcaa6753a2e11fb9c56
Deny Dias <deny@macpress.com.br>
2017-11-24 06:18:36 -0200

    Handle 401 on key verification.

--- inc/admin_settings_license.php
+++ inc/admin_settings_license.php
@@ -6,8 +6,7 @@ function ao_ccss_validate_key($key) {
   // Attach wpdb
   global $wpdb;
 
-  // Set a default validation status and get cached key status
-  $status        = FALSE;
+  // Get cached key status
   $cached_status = get_transient("autoptimize_ccss_key_status_" . md5($key));
 
   // No key validation stored, let's validate it
@@ -21,43 +20,53 @@ function ao_ccss_validate_key($key) {
         'authorization' => 'JWT ' . $key
       ),
       // Body must be JSON
-      'body' => json_encode(array(
-        'url' => get_site_url() . '/',
-        'aff' => 1
-      ))
+      'body' => json_encode(
+        array(
+          'url' => get_site_url() . '/',
+          'aff' => 1
+        )
+      )
     );
 
-    // Dispatch request and store response code (200 is valid, 401 is unauthorized)
+    // Dispatch the request and store its response code
     $response = wp_remote_post($url, $args);
     $code     = $response['response']['code'];
 
-    // Response code is valid
+    // Response code is ok (200)
     if ($code == 200) {
 
       // Cache key status for 1 day
       set_transient("autoptimize_ccss_key_status_" . md5($key), 1, DAY_IN_SECONDS);
 
-      // Set valid key flag
+      // Set validated key status
       $status  = 'validated';
       $class   = 'notice notice-success is-dismissible';
       $message = __('Nice! Your criticalcss.com API key is valid.', 'autoptimize');
 
-    // Response code is invalid
-    } else {
+    // Response code is unauthorized (401)
+    } elseif ($code == 401) {
 
       // Delete cached status for the key
       delete_transient("autoptimize_ccss_key_status_" . md5($key), 1, DAY_IN_SECONDS);
 
-      // Set valid key flag
+      // Set invalid key status
       $status  = 'invalid';
-      $class   = 'notice notice-warning';
+      $class   = 'notice notice-error';
       $message = __('Your API key is invalid, please check again in criticalcss.com.', 'autoptimize');
+
+    // Other response codes
+    } else {
+
+      // Set remote error status
+      $status  = 'error';
+      $class   = 'notice notice-error';
+      $message = __('Something went wrong validating your criticalcss.com. API key. Please try again later.', 'autoptimize');
     }
 
   // Key is still valid
   } elseif ($cached_status && $key) {
 
-    // Set valid key flag
+    // Set valid key status
     $status  = 'valid';
     $class   = 'notice notice-info is-dismissible';
     $message = __('Nice! Your criticalcss.com API key is still valid.', 'autoptimize');
@@ -65,15 +74,17 @@ function ao_ccss_validate_key($key) {
   // No key nor status
   } else {
 
-    // Set no key status
-    $status  = 'nokey';
-    $class   = 'notice notice-error';
-    $message = __('You need to enter a valid criticalcss.com API key to use this Power-Up.', 'autoptimize');
+    // Delete cached status for all keys
     $wpdb->query("
       DELETE FROM $wpdb->options
       WHERE option_name LIKE ('_transient_autoptimize_ccss_key_status_%')
          OR option_name LIKE ('_transient_timeout_autoptimize_ccss_key_status_%')
     ");
+
+    // Set no key status
+    $status  = 'nokey';
+    $class   = 'notice notice-warning';
+    $message = __('You need to enter a valid criticalcss.com API key to use this Power-Up.', 'autoptimize');
   }
 
   // Render license panel
