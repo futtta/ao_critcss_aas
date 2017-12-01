3de17b8d649bdfa391ed4e848b2e5c8f380c6b1d
Deny Dias <deny@macpress.com.br>
2017-12-01 07:14:18 -0200

    Typos.

--- inc/admin_settings_adv.php
+++ inc/admin_settings_adv.php
@@ -28,7 +28,7 @@ function ao_ccss_render_adv() {
               <label for="autoptimize_ccss_vw"><?php _e('Width', 'autoptimize'); ?>:</label> <input type="number" id="autoptimize_ccss_vw" name="autoptimize_ccss_viewport[w]" min="800" max="4096" placeholder="1300" value="<?php echo $viewport['w']; ?>">&nbsp;&nbsp;
               <label for="autoptimize_ccss_vh"><?php _e('Height', 'autoptimize'); ?>:</label> <input type="number" id="autoptimize_ccss_vh" name="autoptimize_ccss_viewport[h]" min="600" max="2160" placeholder="900" value="<?php echo $viewport['h']; ?>">
               <p class="notes">
-                <?php _e('<a href="https://criticalcss.com/account/api-keys" target="_blank">criticalcss.com</a> default viewport size is 1300x900 pixels (width x height). You can change this size by entering desired width and height values above. Allowed values are 800 to 4096 for width, 600 to 2160 for height.', 'autoptimize'); ?>
+                <?php _e('<a href="https://criticalcss.com/account/api-keys" target="_blank">criticalcss.com</a> default viewport size is 1300x900 pixels (width x height). You can change this size by typing a desired width and height values above. Allowed values are 800 to 4096 for width, 600 to 2160 for height.', 'autoptimize'); ?>
               </p>
             </td>
           </tr>
@@ -39,7 +39,7 @@ function ao_ccss_render_adv() {
             <td>
               <input type="checkbox" id="autoptimize_ccss_debug" name="autoptimize_ccss_debug" value="1" <?php checked(1 == $ao_ccss_debug); ?>>
               <p class="notes">
-                <?php _e('<strong>CAUTION! DO NOT use debug mode on production/live environments.</strong><br />Check the box above to enable Autoptimize CriticalCSS Power-Up debug mode. It provides debug facilities in this screen, to <code>/wp-ontent/cache/debug.log</code> file and to the browser console.', 'autoptimize'); ?>
+                <?php _e('<strong>CAUTION! DO NOT use debug mode on production/live environments.</strong><br />Check the box above to enable Autoptimize CriticalCSS Power-Up debug mode. It provides debug facilities in this screen, to <code>/wp-content/cache/ao_ccss/debug.log</code> file and to the browser console.', 'autoptimize'); ?>
               </p>
             </td>
           </tr>
